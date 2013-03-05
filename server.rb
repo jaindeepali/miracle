fbf2c778d6c5a542dffb010c8dff54cb48777874
jaindeepali <djdeepalijain811@gmail.com>
2013-03-06 01:08:00 +0530

    very basic server

--- server.rb
+++ server.rb
@@ -1,23 +1,8 @@
-#!/usr/bin/ruby
 require 'socket'
 puts "Starting up server..."
-# establish the server
-## Server established to listen for connections on port 2008
-server = TCPServer.new('localhost',2008)
-# setup to listen and accept connections
+server = TCPServer.new('0.0.0.0',2008)
 while (session = server.accept)
    request = session.gets
-   #trimmedrequest = request.gsub(/GET\ \//, '').gsub(/\ HTTP.*/, '')
-   #filename = trimmedrequest.chomp
-   #  filename = "index.html"
-   #end
-   session.puts request
-   session.puts "Server: Welcome #{session.peeraddr[2]}\n"
-  # begin
-   #  content = displayfile.read()
-    #  session.print content
-   #rescue Errno::ENOENT
-    #  session.print "File not found"
-   #end
+   session.puts "\n\n\n                         HELLO WORLD!!\n\n\n"
    session.close
-end   #end loop
+end 
