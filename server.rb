c2bf3420fc44db83c3db39759ebae674079bb346
jaindeepali <djdeepalijain811@gmail.com>
2013-03-06 13:07:56 +0530

    added comments

--- server.rb
+++ server.rb
@@ -1,27 +1,54 @@
-require 'socket'
+#server Miracle for Greeting the World!
+
+require 'socket' 
+#ruby socket library
+
+
 puts "Starting up server..."
-server = TCPServer.new('0.0.0.0',2008)
+
+
+server = TCPServer.new('0.0.0.0',2000)
+#server established for listening on port number 2000
+
+
 while (session = server.accept)
+#listening and accepting connection
+
    request = session.gets
-   response = "<h1>HELLO WORLD!!</h1>"
+   #reading request
+
    time=Time.new
+   #current time
+
    puts request
+   #showing request in terminal log
+
    headers = ["HTTP/1.1 200 OK",
              "Server: Miracle",
              "Content-Type: text/html; charset=iso-8859-1",
              "Date: "+ time.inspect+"\r\n\r\n"].join("\r\n")
    session.puts headers
-   trimmedrequest = request.gsub(/GET\ \//, '').gsub(/\ HTTP.*/, '')
-   filename = trimmedrequest.chomp
+   #putting appropriate headers
+
+   filename = request.gsub(/GET\ \//, '').gsub(/\ HTTP.*/, '').chomp
+   #extracting filename from request
+
    if filename == ""
       filename = "index.html"
    end
+   #setting default filename
+
    begin
-      displayfile = File.open(filename, 'r')
-      content = displayfile.read()
+      showfile = File.open(filename, 'r')
+      content = showfile.read()
       session.puts content
    rescue Errno::ENOENT
       session.puts "File not found"
    end
+   #opening and displaying file
+
    session.close
+   #closing session
+
 end 
+#end of loop
