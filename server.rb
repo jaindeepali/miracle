433e5a5782414e1cc9729c954a405c84f7be3308
jaindeepali <djdeepalijain811@gmail.com>
2013-03-06 12:02:20 +0530

    added response headers

--- server.rb
+++ server.rb
@@ -3,6 +3,15 @@ puts "Starting up server..."
 server = TCPServer.new('0.0.0.0',2008)
 while (session = server.accept)
    request = session.gets
-   session.puts "\n\n\n                         HELLO WORLD!!\n\n\n"
+   response = "<h1>HELLO WORLD!!</h1>"
+   time=Time.new
+   puts request
+   headers = ["HTTP/1.1 200 OK",
+             "Server: Ruby",
+             "Content-Type: text/html; charset=iso-8859-1",
+             "Content-Length: response.length",
+             "Date: "+time.inspect+"\r\n\r\n"].join("\r\n")
+   session.puts headers  
+   session.puts response
    session.close
 end 
