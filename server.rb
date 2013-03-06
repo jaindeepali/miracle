3973689982262cfe0e7130ed7a70f3ce6da4fe1d
jaindeepali <djdeepalijain811@gmail.com>
2013-03-06 12:24:31 +0530

    modified response headers and added index.html

--- server.rb
+++ server.rb
@@ -9,9 +9,20 @@ while (session = server.accept)
    headers = ["HTTP/1.1 200 OK",
              "Server: Ruby",
              "Content-Type: text/html; charset=iso-8859-1",
-             "Content-Length: response.length",
-             "Date: "+time.inspect+"\r\n\r\n"].join("\r\n")
-   session.puts headers  
-   session.puts response
+             #{}"Content-Length: #{response.length}",
+             "Date: "+ time.inspect+"\r\n\r\n"].join("\r\n")
+   session.puts headers
+   trimmedrequest = request.gsub(/GET\ \//, '').gsub(/\ HTTP.*/, '')
+   filename = trimmedrequest.chomp
+   if filename == ""
+      filename = "index.html"
+   end
+   begin
+      displayfile = File.open(filename, 'r')
+      content = displayfile.read()
+      session.puts content
+   rescue Errno::ENOENT
+      session.puts "File not found"
+   end
    session.close
 end 
